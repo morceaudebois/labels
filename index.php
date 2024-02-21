746684f3c8dbf9dc8fb6e3c90ae022928df4a248
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2024-02-21 18:31:21 +0100

    Only include svgs

--- index.php
+++ index.php
@@ -6,14 +6,18 @@ function echoFiles() {
     // Get the list of files and directories in the current directory
     $files = scandir(__DIR__);
 
-    // Define files to exclude
-    $excluded_files = array('.git', 'index.php', '.DS_Store', 'source.sketch', '.gitignore');
     // Iterate over the array of files
     foreach ($files as $file) {
         // Exclude the current directory (.) and parent directory (..)
-        if ($file != '.' && $file != '..' && !in_array($file, $excluded_files)) {
-            // Display the file as an image with a clickable link
-            echo '<a href="' . htmlspecialchars($file) . '"><img style="height: 65px; margin-right: 1em; margin-bottom: 1em;" src="' . htmlspecialchars($file) . '" alt="' . htmlspecialchars($file) . '"></a>';
+        if ($file != '.' && $file != '..') {
+            // Get the file extension
+            $file_extension = pathinfo($file, PATHINFO_EXTENSION);
+
+            // Check if the file has a .svg extension
+            if (strtolower($file_extension) === 'svg') {
+                // Display the file as an image with a clickable link
+                echo '<a href="' . htmlspecialchars($file) . '"><img style="height: 65px; margin-right: 1em; margin-bottom: 1em;" src="' . htmlspecialchars($file) . '" alt="' . htmlspecialchars($file) . '"></a>';
+            }
         }
     }
 }
@@ -21,3 +25,5 @@ function echoFiles() {
 
 
 <div class="container"><?php echoFiles() ?></div>
+
+<a href="https://github.com/morceaudebois/labels" style="margin-top: 4em; display: block;">Source 🌊</a>
